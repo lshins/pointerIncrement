47d0101bcc8767c7070f7df1ee846dd651b2aebc
lshinabery02@gmail.com <lshinabery02@gmail.com>
2020-05-16 12:34:50 -0400

    complete

--- Source.cpp
+++ Source.cpp
@@ -3,21 +3,18 @@
 
 using namespace std;
 
-const void print(long int**&, const int);
+const void print(long int*, const int);
 
 const int dim = 10;
 
 int main()
 {
 	long int nums[dim];
-	long int** adds;
-	adds = new long int* [dim];
-	*adds = new long int[0];
+	long int* adds = new long int [dim];
 	for (int i = 0; i < dim; i++)
 	{
 		nums[i] = i + 1;
-		adds[i] = &nums[i];
-		adds[i][0] = nums[i];
+		adds = &nums[i];
 		adds++;
 	}
 	for (int i = 0; i < dim; i++)
@@ -26,9 +23,9 @@ int main()
 	return 0;
 }
 
-const void print(long int**& a, const int d)
+const void print(long int* a, const int d)
 {
 	for (int i = 0; i < d; i++)
-		cout << &a[i][0] << " " << showpoint << fixed << setprecision(1) << a[i][0] << endl;
+		cout << &a[i] << " " << showpoint << fixed << setprecision(1) << a[i] << endl;
 	cout << endl;
 }
